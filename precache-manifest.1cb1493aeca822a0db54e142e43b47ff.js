2e6001402f5fddd157083d7c63c458f8d61026fa
devshaffan <devshaffan@gmail.com>
2021-11-20 22:24:55 +0500

    Updates

---
+++ precache-manifest.1cb1493aeca822a0db54e142e43b47ff.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "f6cb02c8f53a17c4851c",
+    "url": "/HTO/static/css/main.6e652889.chunk.css"
+  },
+  {
+    "revision": "f6cb02c8f53a17c4851c",
+    "url": "/HTO/static/js/main.882c223f.chunk.js"
+  },
+  {
+    "revision": "60a43e26a8d3699af44a",
+    "url": "/HTO/static/js/runtime~main.330f60c9.js"
+  },
+  {
+    "revision": "6414552e1f111aac3d80",
+    "url": "/HTO/static/css/2.551c9e84.chunk.css"
+  },
+  {
+    "revision": "6414552e1f111aac3d80",
+    "url": "/HTO/static/js/2.1b34da3d.chunk.js"
+  },
+  {
+    "revision": "a73954d12550782b823ef827c6d3cc04",
+    "url": "/HTO/static/media/cartR_H.a73954d1.png"
+  },
+  {
+    "revision": "d08c52463318dd0ee1dffa8b658fef8d",
+    "url": "/HTO/static/media/cartR_T.d08c5246.png"
+  },
+  {
+    "revision": "9453a215e8569bc0d48dd800f29684ba",
+    "url": "/HTO/static/media/cartL_H.9453a215.png"
+  },
+  {
+    "revision": "3a59e185a90898933e52f25349ae4d7d",
+    "url": "/HTO/static/media/cartL_T.3a59e185.png"
+  },
+  {
+    "revision": "efef2d82cf68e13a2f73dc6733505d08",
+    "url": "/HTO/static/media/arrow.efef2d82.gif"
+  },
+  {
+    "revision": "af18e021df0acbf0ed1934fd8437f500",
+    "url": "/HTO/static/media/background.af18e021.jpg"
+  },
+  {
+    "revision": "7360a269fa89ec8922a9fb9354820523",
+    "url": "/HTO/static/media/sound.7360a269.png"
+  },
+  {
+    "revision": "424d18986561c7039221eb97a66aef6b",
+    "url": "/HTO/static/media/tostartpressbutton.424d1898.mp3"
+  },
+  {
+    "revision": "8efff185f64e4259ae68a4074dbfb0e6",
+    "url": "/HTO/static/media/321go.8efff185.mp3"
+  },
+  {
+    "revision": "09b0b7bb4e8aad40481825ec9d91d223",
+    "url": "/HTO/static/media/rightAnswer.09b0b7bb.mp3"
+  },
+  {
+    "revision": "ddcbc59b2917e3dbeb737fca1e3ede4e",
+    "url": "/HTO/static/media/wrongAnswer.ddcbc59b.mp3"
+  },
+  {
+    "revision": "e0e17d693f0783d24c29a0c8383fc411",
+    "url": "/HTO/static/media/cartR_O.e0e17d69.png"
+  },
+  {
+    "revision": "722a23b1191d46966aedd78d95fca9d8",
+    "url": "/HTO/static/media/cartL_O.722a23b1.png"
+  },
+  {
+    "revision": "5d448f98f7dd2c51dd322bd52424ed03",
+    "url": "/HTO/static/media/arrowL.5d448f98.gif"
+  },
+  {
+    "revision": "59843b2d65b09b144fd402b9327e37d1",
+    "url": "/HTO/static/media/instructions.59843b2d.gif"
+  },
+  {
+    "revision": "a0d6b13874a29393b0f8252f2adb5fa7",
+    "url": "/HTO/static/media/grocerySound.a0d6b138.mp3"
+  },
+  {
+    "revision": "f02ac89a44b28acd80cb6b0399adbf8f",
+    "url": "/HTO/static/media/mute.f02ac89a.png"
+  },
+  {
+    "revision": "3be6a397407fcad666cc42012a0d147b",
+    "url": "/HTO/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/HTO/precache-manifest.1cb1493aeca822a0db54e142e43b47ff.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/HTO/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
